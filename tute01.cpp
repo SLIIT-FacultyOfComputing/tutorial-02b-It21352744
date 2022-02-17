1a4b851477f5ca4911c22e21345e86d8c843ad71
IT21352744 <99311994+It21352744@users.noreply.github.com>
2022-02-17 11:13:46 +0000

    Completed

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,16 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include<iostrem>
+useing namespace std;
 int main() 
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+    int cm, inches;
+    cout<<"Enter a length in cm :";
+    cin>>"cm";
+   
     inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    cout<<setioflags(ios::fixed)<<setprecision(3)<<inches<<end;
+    
     return 0;
 }  
